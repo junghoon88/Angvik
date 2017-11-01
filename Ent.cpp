b92df59b062db1d5ebfa2c7f4fbf9dead7b4a807
Parkhyunjun <hyunjun4494@gmail.com>
2017-11-01 16:34:06 +0900

    갱신 + 에너미 테스트

--- Ent.cpp
+++ Ent.cpp
@@ -15,7 +15,7 @@ Ent::~Ent()
 void Ent::init(float x, float y)
 {
 	spt = IMAGEMANAGER->findImage(L"나무맨");
-
+	spt->setCoord({ 0,0 });
 	//마더클래스 프로브축 추가해야함.
 	dir = eRIGHT;
 
@@ -23,25 +23,43 @@ void Ent::init(float x, float y)
 	ptX = x;
 	ptY = y;
 
-	rc = RectMakeCenter(x, y, 50, 70);
-
+	rc = RectMakeCenter(x, y, 50, 80);
 	probeY = rc.bottom;
 }
 void Ent::update(void)
 {
-	rc = RectMakeCenter(ptX, ptY, 50, 70);
+	rc = RectMakeCenter(ptX, ptY, 50, 80);
 	probeY = rc.bottom;
+	spt->setCoord({ ptX,ptY });
+
+	frameTime += TIMEMANAGER->getElapsedTime();
+	if (frameTime >= 0.1f)
+	{
+		frameTime -= 0.2f;
+
+		frameCnt++;
+		if (frameCnt >= 8) frameCnt = 0;
+	}
 
 	move();
 }
 void Ent::render(void)
 {
-
+	spt->frameRender(frameCnt, 0, true);
 }
 void Ent::move(void)  
 {
-	/*
-	for (int i = probeY - 3; i < probeY; ++i)
+	
+	if (dir == eLEFT)
+	{
+		ptX -= 5;
+	}
+	else if (dir == eRIGHT)
+	{
+		ptX += 5;
+	}
+
+	for (int i = probeY - 10; i < probeY + 10; ++i)//Y축 탐지
 	{
 		COLORREF color = PBGMANAGER->getPixelColor(L"테스트배경", ptX, i);
 
@@ -49,13 +67,31 @@ void Ent::move(void)
 		int g = GetGValue(color);
 		int b = GetBValue(color);
 
-		if ((r == 255 && g == 0 && b == 0)) // 관통형 바닥
+		if ((r == 0 && g == 0 && b == 0)) // 관통형 바닥
 		{
-			ptY = i + 1;
+			ptY = i - 40;
 			break;
 		}
 	}
-	*/
+	//Y축 탐지
+
+	for (int i = ptX - 20; i < ptX + 20; ++i) // x축 탐지
+	{
+		COLORREF color = PBGMANAGER->getPixelColor(L"테스트배경", i, ptY);
+
+		int r = GetRValue(color);
+		int g = GetGValue(color);
+		int b = GetBValue(color);
+
+		if ((r == 0 && g == 255 && b == 255)) // 관통형 바닥
+		{
+			if (i > ptX) dir = eLEFT;
+			else if (i < ptX) dir = eRIGHT;
+			break;
+		}
+	}
+	// x축 탐지
+	
 }
 void Ent::attack(void)
 {
--- sceneInit.cpp
+++ sceneInit.cpp
@@ -166,6 +166,6 @@ void sceneInit::enemyInitImage(void)	//몬스터
 	IMAGEMANAGER->addFrameImage(DEVICE, L"나무맨", L"image/monster/Ent_move_6f.png", 6, 1);
 	IMAGEMANAGER->addFrameImage(DEVICE, L"콩나물", L"image/monster/kong_idle_6f.png", 6, 1);
 	IMAGEMANAGER->addFrameImage(DEVICE, L"콩나물어택", L"image/monster/kong_atk_8f.png", 8, 1);
-	PBGMANAGER->addImage(L"테스트배경", L"image/stage/test_collision.png",768,305);
-
+	PBGMANAGER->addImage(L"테스트배경", L"image/stage/test_bmp.bmp",768,305);
+	IMAGEMANAGER->addImage(DEVICE,L"테스트배경", L"image/stage/test_collision.png");
 }
