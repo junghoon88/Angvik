d856c01c0fca36c7421d1ea92235bbe223de735d
hanul <skough0217@gmail.com>
2017-11-04 13:44:52 +0900

    no message

--- inven.h
+++ inven.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "DxWindow.h"
+#include "Item.h"
 
 class inven : public DxWindow
 {
 private:
+	ITEM_TYPE _itemType;
+	ITEM_KIND _itemKind;
+	ITEM_STATE _itemState;
+
 	bool _isMenew;
 	bool _isItem;
 	bool _isOils;
@@ -17,5 +22,7 @@ public:
 	void update(void);
 	void update(float x, float y);
 	void render(void);
+
+	ITEM_TYPE getItemType(void) { return _itemType; }
 };
 
