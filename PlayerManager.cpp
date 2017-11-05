3a6c3a87e4a472d5e201227b4424efac3fee2659
Parkhyunjun <hyunjun4494@gmail.com>
2017-11-05 19:58:20 +0900

    픽셀충돌 맵 약간 수정

--- PlayerManager.cpp
+++ PlayerManager.cpp
@@ -308,8 +308,8 @@ void PlayerManager::playerItemCollision(void)
 	Collision();
 
 
-	_player->update();
-	_bird->update(_player->getX(), _player->getY());
+	//_player->update();
+	//_bird->update(_player->getX(), _player->getY());
 
 
 }
