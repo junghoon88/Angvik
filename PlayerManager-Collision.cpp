affc770443b7fc1215a563d86f7a04768d48bdef
이동희 <veritas811@naver.com>
2017-11-06 01:03:48 +0900

    플레이어피격신호전달

--- PlayerManager-Collision.cpp
+++ PlayerManager-Collision.cpp
@@ -9,7 +9,8 @@ void PlayerManager::Collision(void)
 	{
 		if (isCollision(_em->getKongtan()->getVBullet()[i].rc,
 			_player->getRectBody()))    //플레이어피해받음
-		{
+		{	
+			_player->setIsHit(true);    //피해신호
 			_em->getKongtan()->remove(i);
 			//아이템에 신호 넘겨줘야함
 			break;
@@ -23,6 +24,7 @@ void PlayerManager::Collision(void)
 		if (isCollision(_em->getSbmr()->getVBullet()[i].rc,
 			_player->getRectBody())) //플레이어피해받음
 		{
+			_player->setIsHit(true);
 			_em->getSbmr()->remove(i);
 			//아이템에 신호 넘겨줘야함
 
@@ -34,15 +36,9 @@ void PlayerManager::Collision(void)
 	for (int i = 0; i < _em->getvEnemy().size(); i++)   //플레이어 몬스터충돌처리
 	{	
 		RECT temp2;
-<<<<<<< HEAD
+
 		if (IntersectRect(&temp2, &_player->getRectBody(), &_em->getvEnemy()[i]->getRect())) {     //플레이어 피해받음
-				
-=======
-		if (IntersectRect(&temp2, &_player->getRectBody(), &_em->getvEnemy()[i]->getRect())) { //플레이어 피해받음
-			
-
->>>>>>> bbeb63271bdf79d348f48e443c033fa1d4b29388
-
+				_player->setIsHit(true);
 		}
 	
 		RECT temp;
