fffcd81a7805a259e5435b43c0fbe5fc0b120997
한규현 <hki8588@hanmail.net>
2017-11-06 02:34:59 +0900

    아이템 내구도 시스템 구현, 아이템 위치 보정

--- PlayerManager-Collision.cpp
+++ PlayerManager-Collision.cpp
@@ -5,7 +5,7 @@
 
 void PlayerManager::Collision(void)
 {
-	for (int i = 0; i < _em->getKongtan()->getVBullet().size();i++)
+	for (int i = 0; i < _em->getKongtan()->getVBullet().size(); i++)
 	{
 		if (isCollision(_em->getKongtan()->getVBullet()[i].rc,
 			_player->getRectBody()))
@@ -14,7 +14,7 @@ void PlayerManager::Collision(void)
 			//아이템에 신호 넘겨줘야함
 			break;
 		}
-	
+
 	} // 콩탄
 
 	for (int i = 0; i < _em->getSbmr()->getVBullet().size(); i++)
@@ -32,49 +32,58 @@ void PlayerManager::Collision(void)
 
 
 	for (int i = 0; i < _em->getvEnemy().size(); i++)   //플레이어 몬스터충돌처리
-	{	
-		RECT temp2;
-<<<<<<< HEAD
-		if (IntersectRect(&temp2, &_player->getRectBody(), &_em->getvEnemy()[i]->getRect()) && !_player->getIsJump()) {     //플레이어 피해받음
-	
-=======
-		if (IntersectRect(&temp2, &_player->getRectBody(), &_em->getvEnemy()[i]->getRect())) {     //플레이어 피해받음
-			
->>>>>>> d6290db2400c0d17fe3f0b066faf826cc3612453
-
-		}
-		RECT temp;
-		if (IntersectRect(&temp, &_player->getRectFoot(), &_em->getvEnemy()[i]->getRect()) && _player->getIsJump()) {    //플레이어가 적밟음
-			_em->getvEnemy()[i]->setLife((_em->getvEnemy()[i]->getLife()) - 1);   //life -1
-	
-			
-		}
-	}
-	for (int i = 0; i < _em->getvEnemy().size(); i++)   //플레이어 무기 몬스터
 	{
-		for(int j = 0; j<_im->getVItem().size(); j++)
-		{	RECT temp;
-			
-			if (_im->getVItem()[j]->getState() == ITEM_STATE_ATTACK)
-			{
-				if (IntersectRect(&temp, &_im->getVItem()[j]->getHitImg(), &_em->getvEnemy()[i]->getRect()))
-				{    //플레이어가 무기씀
-					_em->getvEnemy()[i]->setLife((_em->getvEnemy()[i]->getLife()) - 1);   //life -1
-<<<<<<< HEAD
-					if (_im->getVItem()[j]->getType() == ITEM_TYPE_LANCE) _im->getVItem()[j]->setState(ITEM_STATE_IDLE);
-					if (_em->getvEnemy()[i]->getLife() <= 0)
-					{
-						_im->setItem(_em->getvEnemy()[i]->getX(), _em->getvEnemy()[i]->getY() - 30);
-						_em->deleteEnemy(i);
-						break;
-					}
-=======
+		RECT temp2;
 
-				
->>>>>>> d6290db2400c0d17fe3f0b066faf826cc3612453
-				}
+		if (IntersectRect(&temp2, &_player->getRectBody(), &_em->getvEnemy()[i]->getRect()) && !_player->getIsJump()) {     //플레이어 피해받음
+
+
+			if (IntersectRect(&temp2, &_player->getRectBody(), &_em->getvEnemy()[i]->getRect())) {     //플레이어 피해받음
+
+
+
 			}
-			
+			RECT temp;
+			if (IntersectRect(&temp, &_player->getRectFoot(), &_em->getvEnemy()[i]->getRect()) && _player->getIsJump()) {    //플레이어가 적밟음
+				_em->getvEnemy()[i]->setLife((_em->getvEnemy()[i]->getLife()) - 1);   //life -1
+
+
+			}
+		}
+		for (int i = 0; i < _em->getvEnemy().size(); i++)   //플레이어 무기 몬스터
+		{
+			for (int j = 0; j < _im->getVItem().size(); j++)
+			{
+				RECT temp;
+
+				if (_im->getVItem()[j]->getState() == ITEM_STATE_ATTACK)
+				{
+					if (IntersectRect(&temp, &_im->getVItem()[j]->getHitImg(), &_em->getvEnemy()[i]->getRect()))
+					{    //플레이어가 무기씀
+						_im->getVItem()[j]->setdurability(_im->getVItem()[j]->getdurability() - 1);
+						_em->getvEnemy()[i]->setLife((_em->getvEnemy()[i]->getLife()) - 1);   //life -1
+
+						if (_im->getVItem()[j]->getType() == ITEM_TYPE_LANCE)
+						{
+							MYPOINT  pt;
+							pt.x = _em->getvEnemy()[i]->getX()-10;
+							pt.y = _em->getvEnemy()[i]->getY()-30;
+							_im->getVItem()[j]->setPt(pt);
+							
+							_im->getVItem()[j]->setState(ITEM_STATE_IDLE);
+							
+						}
+						if (_em->getvEnemy()[i]->getLife() <= 0)
+						{
+							_im->setItem(_em->getvEnemy()[i]->getX(), _em->getvEnemy()[i]->getY() - 30);
+							_em->deleteEnemy(i);
+							break;
+						}
+						
+					}
+				}
+
+			}
 		}
 	}
 }
